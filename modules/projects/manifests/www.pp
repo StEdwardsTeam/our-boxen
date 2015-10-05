e6f8439f7d57db4c3f49e29794f9e55dd01e9b1f
Paul Bailey <paulba@stedwards.edu>
2015-10-05 07:49:02 -0500

    update www url

--- modules/projects/manifests/www.pp
+++ modules/projects/manifests/www.pp
@@ -3,7 +3,7 @@ class projects::www {
   include php::composer
 
   php::project { 'www':
-    source        => 'StEdwardsTeam/freshform_www',
+    source        => 'StEdwardsTeam/www',
     mysql         => true,
     nginx         => 'projects/shared/drupal.nginx.conf.erb',
     php           => '5.4.41',
