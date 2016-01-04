aa9b6b45b2f0707e76e73244ba8a08ca426c7a22
Paul Bailey <paulba@stedwards.edu>
2016-01-04 08:42:31 -0600

    bump php for www to 5.6.15

--- modules/projects/manifests/www.pp
+++ modules/projects/manifests/www.pp
@@ -2,20 +2,22 @@ class projects::www {
 
   include php::composer
 
+  $php_version = '5.6.15'
+
   php::project { 'www':
     source        => 'StEdwardsTeam/www',
     mysql         => true,
     nginx         => 'projects/shared/drupal.nginx.conf.erb',
-    php           => '5.4.41',
+    php           => $php_version,
   }
 
-  php::extension::imagick { 'imagick for 5.4.41':
-    php     => '5.4.41',
+  php::extension::imagick { "imagick for ${php_version}":
+    php     => $php_version,
     version => '3.1.2'
   }
 
-  php::extension::xdebug { 'xdebug for 5.4.41':
-    php     => '5.4.41',
+  php::extension::xdebug { 'xdebug for ${php_version}':
+    php     => $php_version,
     version => '2.3.3'
   }
 }
