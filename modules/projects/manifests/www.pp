bfa30b26a556db8c76fdb473f20eb4a3f22ddfac
Paul Bailey <paulba@stedwards.edu>
2016-01-04 09:29:32 -0600

    Revert "idempotency fix"
    This reverts commit 933146ece722390ea28f9540aa8eebecbed835a3.

--- modules/projects/manifests/www.pp
+++ modules/projects/manifests/www.pp
@@ -9,11 +9,13 @@ class projects::www {
     mysql         => true,
     nginx         => 'projects/shared/drupal.nginx.conf.erb',
     php           => $php_version,
-  } ->
+  }
+
   php::extension::imagick { "imagick for ${php_version}":
     php     => $php_version,
     version => '3.1.2'
-  } ->
+  }
+
   php::extension::xdebug { 'xdebug for ${php_version}':
     php     => $php_version,
     version => '2.3.3'
