1605a4b7d45806ecdc163b71307257cbdc9710b3
Paul Bailey <paulba@stedwards.edu>
2016-03-18 14:51:44 -0500

    add src dir deps

--- modules/people/manifests/milesperryseu.pp
+++ modules/people/manifests/milesperryseu.pp
@@ -2,4 +2,13 @@ class people::milesperryseu {
 
   include projects::forms
 
+  $home = "/Users/${::boxen_user}"
+
+  file { "${home}/dev":
+    ensure => directory,
+  } ->
+  file { "${home}/dev/apps":
+    ensure => directory,
+    before => File["${home}/dev/apps/projects"]
+  }
 }
