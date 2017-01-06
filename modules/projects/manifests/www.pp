c0bfca9a60c8fbb94340d477644b85f39a983595
Paul Bailey <paulba@stedwards.edu>
2017-01-06 13:19:39 -0600

    add drupal local settings

--- modules/projects/manifests/www.pp
+++ modules/projects/manifests/www.pp
@@ -2,6 +2,7 @@ class projects::www {
 
   require projects::drupal_7
 
+  $project_name = 'www'
   $php_version = hiera(drupal7::php_version)
 
   php::project { 'www':
@@ -9,5 +10,13 @@ class projects::www {
     mysql         => true,
     nginx         => 'projects/shared/drupal.nginx.conf.erb',
     php           => $php_version,
+  } ->
+  file { "${::boxen::config::srcdir}/${project_name}/src/sites/default/settings-local.php":
+    ensure  => file,
+    mode    => '0644',
+    replace => false,
+    content => template('projects/shared/drupal.settings-local.php.erb'),
+    owner   => $::boxen_user,
+    group   => $::boxen_group,
   }
 }
